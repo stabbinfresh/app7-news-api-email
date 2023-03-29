c4ee6d6c4a188842584ccf6bb65c0355bccfbb72
Andrew Kuhs <andythekuhs@gmail.com>
2023-03-29 02:36:00 -0400

    changed url

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import requests
 from send_email import send_email
 
+
+topic = "palantir"
 api_key = "ac8bcd9c0c454bc886e75a9c172fa9a4"
 
-# put the date back a few weeks before
-# the current date for this url to work
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2023-03-10&sortBy=publishedAt&apiKey=" \
-      "ac8bcd9c0c454bc886e75a9c172fa9a4"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "sortBy=publishedAt&" \
+      "apiKey=ac8bcd9c0c454bc886e75a9c172fa9a4&" \
+      "language=en"
 
 # Make request
 request = requests.get(url)
@@ -15,11 +17,18 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-# Access the article titles and description
-body = ""
-for article in content["articles"]:
-    if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+# Check if articles key exists
+if "articles" in content:
+    # Access the article titles and description
+    body = ""
+    for article in content["articles"][:30]:
+        if article["title"] is not None:
+            body = "Subject: Today's News" + "\n"\
+                   + body + article["title"] + "\n" \
+                   + article["description"] + "\n" \
+                   + article["url"] + 2 * "\n"
 
-body = body.encode("utf-8")
-send_email(message=body)
+    body = body.encode("utf-8")
+    send_email(message=body)
+else:
+    print("No articles found.")
